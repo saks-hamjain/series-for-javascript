cafc4346fab97e38c09d44d903b4dd3bfdb6feb0
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-04-04 05:18:29 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountNo = 741963258
+let email = "Vatsal@gmail.com"
+var name = "Tanish"
+city  = "Kanpur"
+// accountNo = 8529987
+console.log(accountNo);
+console.log([accountNo,email,name,city]);
+
