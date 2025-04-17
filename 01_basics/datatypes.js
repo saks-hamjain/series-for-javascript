d373553256185f79b5ef69b36beaf793097e4294
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-04-17 19:03:01 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all the cose as newer version of js
+
+// alert("Hey you are dead");  we are using nodejs not browser
+let num = 25
+//integer ,boolean ,bigint,string 
+// in javascript we also have null datatype
+// null is a standalone value
+// symbol => its used for uniqueness
+// these were primitive datatype 
+// other one is object 
+// typeof is used to get the type of the particular object and datatype
+// typeof null is object 
+console.log(typeof null)
+console.log(typeof undefined)
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -4,5 +4,12 @@ var name = "Tanish"
 city  = "Kanpur"
 // accountNo = 8529987
 console.log(accountNo);
-console.log([accountNo,email,name,city]);
+console.table([accountNo,email,name,city]);
 
+/*
+not to use var because it has 
+issue in block and functional scope
+*/
+
+let accountState
+console.log(accountState)
