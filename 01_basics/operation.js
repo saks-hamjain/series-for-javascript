d51c34702d238324a7af10f1e2a4c5a1771c82d8
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-04-17 19:55:28 +0000

    operation

---
+++ 01_basics/operation.js
@@ -0,0 +1,11 @@
+let value =5;
+console.log(-value);
+let str1 = "Saksham";
+let str2 = " Jain"
+console.log(str1+str2)
+console.log(1+"2") // output will be 12 
+console.log(1+2+"2")// output will be 32
+console.log("1"+2+2)//output will be 122
+console.log(+true);//output 1
+console.log(+""); //output 0
+
