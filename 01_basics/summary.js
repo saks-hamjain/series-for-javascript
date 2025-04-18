72c4d48f3035ec2f6df2538fea74d93d4733c5c7
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-04-18 17:33:56 +0000

    summary

---
+++ 01_basics/summary.js
@@ -0,0 +1,13 @@
+//primitive datatypes
+//strings,number,null,boolean,undefined,symbol,
+//these are call by value 
+
+// reference type
+//
+//call by reference 
+// object,arrays
+// functions
+//
+//let func = function(){console.log("HELLO");}
+// let varii = {feio :12
+// name="saksham"}
