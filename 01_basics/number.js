41be3f8818069877e47699fe13615422816cb87f
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-04-19 12:39:16 +0000

    maths

---
+++ 01_basics/number.js
@@ -0,0 +1,8 @@
+const score = 1000
+console.log(score)
+const user= new Number(4566)
+console.log(user);
+
+console.log(user.toString().length);
+console.log(user.toFixed(3));
+console.log(user.toPrecision(6));
---
+++ 01_basics/strings.js
@@ -0,0 +1,10 @@
+let first = "hello";
+let sec = " World";
+console.log(first+sec);
+console.log(`This is string interpolation ${"hey there"} so easy to use ${'????'}`)
+const name = new String("Saksham");
+console.log(name[0]);
+console.log(name.toUpperCase());
+let str = "tanish";
+str.toUpperCase();
+console.log(str.__proto__)
