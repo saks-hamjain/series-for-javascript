8213282d9f02d2d976d8116e4a5beab0363a7604
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-04-19 13:32:11 +0000

    math

---
+++ 01_basics/math.js
@@ -0,0 +1,12 @@
+console.log(Math);
+console.log(Math.abs(-563));
+console.log(Math.round(5.5));
+console.log(Math.ceil(96.55));
+console.log(Math.floor(3.2));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+let max=20;
+let min = 10;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
